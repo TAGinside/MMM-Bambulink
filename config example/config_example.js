5f6e23cd950a391a51fd88af8f072a7cb22a9435
TAGinside <tag.costa@bluewin.ch>
2026-05-16 13:07:39 +0200

    Add configuration example for MMM-Bambulink module

---
+++ config example/config_example.js
@@ -0,0 +1,16 @@
+modules: [
+  {
+  module: "MMM-Bambulink",
+  position: "top_left",
+  config: {
+    ip: "192.168.11.9",
+    accessCode: "1f019aaf",        // LAN Connection Code
+    serial: "01S000XXXX",          // Seial number of the printer
+    mqttPort: 8883,
+    useTLS: true,
+    updateInterval: 5 * 1000,
+    showThumbnail: true ,
+    thumbnailPath: "modules/MMM-Bambulink/public/printer.png",
+    }
+  }
+]
